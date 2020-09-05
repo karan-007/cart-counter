5eb62d3133641fe79a8d26dcdcefae71cf3fa62c
karan chouhan <karan.chouhan@mountblue.tech>
2020-09-05 15:14:43 +0530

    styled delete button

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -4,7 +4,7 @@ import "./buttons.css"
 function buttons(props) {
     return (
         <div className="Counter">
-            <span>{props.value}</span>
+            <span className="itemValue">{props.value}</span>
             <button className="Button ButtonPlus" onClick={() => props.onAdd(props.id)}>+</button>
             <button className="Button ButtonMinus" onClick={() => props.onMinus(props.id)}>-</button>
             <button className="Button ButtonDelete" onClick={() => props.onDelete(props.id)}>Delete</button>
